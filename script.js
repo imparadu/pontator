edd3ad7e0f6aa96aa748d6aed179dab189eb60c0
imparadu <imparadu@gmail.com>
2023-10-27 15:02:54 +0300

    primu js

---
+++ script.js
@@ -0,0 +1,32 @@
+let [seconds,minutes,hours] = [0,0,0];
+let timerRef = document.querySelector(`.counter`);
+let int = null;
+document.getElementById(`startTimer`).addEventListener(`click`, ()=>{
+    if(int!==null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer,10);
+});
+document.getElementById(`pauseTimer`).addEventListener(`click`, ()=>{
+    clearInterval(int);
+});
+document.getElementById(`resetTimer`).addEventListener(`click`, ()=>{
+    clearInterval(int);
+    [seconds,minutes,hours] = [0,0,0];
+    timerRef.innerHTML = `00:00:00`;
+});
+function displayTimer(){
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+ let h = hours < 10 ? `0` + hours : hours;
+ let m = minutes < 10 ? `0` + minutes : minutes;
+ let s = seconds < 10 ? `0` + seconds : seconds;
+
+ timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
